0f0a710929d1dba6c6cc448fe4898e7e123ad0c6
the f <feldrim@gmail.com>
2018-09-03 14:52:37 +0300

    Check for "LiteDbIdentity", if not available pick the first connection string -assuming only one connection string available.

--- AspNetCore.Identity.LiteDB/AspNetCore.Identity.LiteDB/Data/LiteDBContext.cs
+++ AspNetCore.Identity.LiteDB/AspNetCore.Identity.LiteDB/Data/LiteDBContext.cs
@@ -1,12 +1,17 @@
-﻿using LiteDB;
+﻿using System.Linq;
+using LiteDB;
 using Microsoft.Extensions.Configuration;
 
 namespace AspNetCore.Identity.LiteDB.Data
 {
    public class LiteDbContext
    {
-      public LiteDbContext(IConfiguration configuration) =>
-         LiteDatabase = new LiteDatabase(configuration.GetConnectionString("Default"));
+      public LiteDbContext(IConfiguration configuration)
+      {
+         var connectionString = configuration.GetConnectionString("LiteDbIdentity") ?? configuration.GetSection("ConnectionStrings").GetChildren().FirstOrDefault()?.Value;
+
+         LiteDatabase = new LiteDatabase(connectionString);
+      }
 
       public LiteDatabase LiteDatabase { get; set; }
    }
