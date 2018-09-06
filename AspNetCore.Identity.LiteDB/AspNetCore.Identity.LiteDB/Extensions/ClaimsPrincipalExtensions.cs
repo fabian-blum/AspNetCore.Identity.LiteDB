726a2d1165052d96322f1be446e8240a96203cb8
the f <feldrim@gmail.com>
2018-09-06 21:51:06 +0300

    Added IsInRole method with IdentityRole parameter

---
+++ AspNetCore.Identity.LiteDB/AspNetCore.Identity.LiteDB/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace AspNetCore.Identity.LiteDB.Extensions
+{
+   public static class ClaimsPrincipalExtensions
+   {
+      public static bool IsInRole(this ClaimsPrincipal claimsPrincipal, IdentityRole role) => claimsPrincipal.IsInRole(role.NormalizedName);
+   }
+}
