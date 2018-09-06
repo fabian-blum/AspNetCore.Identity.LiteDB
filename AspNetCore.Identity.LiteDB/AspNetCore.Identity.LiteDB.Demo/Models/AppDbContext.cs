b5ce8c9d2ba81440575e60118e41ee14b788a953
the f <feldrim@gmail.com>
2018-09-06 12:07:03 +0300

    Added Added custom database context

---
+++ AspNetCore.Identity.LiteDB/AspNetCore.Identity.LiteDB.Demo/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspNetCore.Identity.LiteDB.Data;
+using LiteDB;
+
+namespace AspNetCore.Identity.LiteDB.Demo.Models
+{
+   /// <summary>
+   ///   Sample DbContext to configure ILiteDbContext with custom implementation
+   /// </summary>
+   public class AppDbContext : ILiteDbContext
+   {
+      public LiteDatabase LiteDatabase { get; } = new LiteDatabase("Filename=Database.db");
+   }
+}
